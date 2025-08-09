aa8e2ea3bf99d2fb1a0015d56371e297d8161173
Sagar <Sagar181203@gmail.com>
2025-08-09 11:06:02 +0530

    Sanajan's first commit

---
+++ first.js
@@ -0,0 +1,42 @@
+let navbar = document.getElementById("navbar");
+navbar.style.display = "flex";
+navbar.style.justifyContent = "space-between";
+navbar.style.alignItems = "center";
+navbar.style.padding = "10px";
+navbar.style.backgroundColor = "#333";
+navbar.style.textDecoration = "none";
+navbar.style.color = "#fff";
+let socialIcons = document.getElementById("social-icons");
+socialIcons.style.display = "flex";
+socialIcons.style.gap = "10px";
+
+
+
+
+let content= document.getElementById("content");
+content.style.display = "flex";
+content.style.justifyContent = "space-between";
+content.style.alignItems = "center";
+content.style.padding = "10px";
+
+
+let text= document.getElementById("text");
+text.style.display = "flex";
+text.style.flexDirection = "column";
+text.style.width = "70%"; 
+text.style.color = "#fff";
+
+let imgcon= document.getElementById("img-con");
+imgcon.style.display = "flex";
+imgcon.style.justifyContent = "center";
+imgcon.style.alignItems = "center";
+imgcon.style.padding = "10px";
+
+let image= document.getElementById("image");
+// image.style.display = "flex";
+// image.style.justifyContent = "center";
+// image.style.alignItems = "center";
+// image.style.padding = "10px";
+image.style.width = "30%";
+image.style.height = "10%";
+image.style.borderRadius = "50%";
